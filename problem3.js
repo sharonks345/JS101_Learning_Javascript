1f42fbfdf569fbf08aedf120d47c2837390cca13
sharonks345 <sharonks1995@gmail.com>
2024-01-09 03:40:31 +0530

    new files added

---
+++ problem3.js
@@ -0,0 +1,8 @@
+// Problem 3
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Concatenate and console them in a new string to get the output like this : ("My name is"+ name +"and my age is"+ age +".")
+
+let name = "Sharon K Sebastian"
+let age = 28;
+console.log("My name is "+name+" and age is "+age)
---
+++ problem4.js
@@ -0,0 +1,23 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+var name = "𝑨𝒌𝒔𝒉𝒂𝒚";
+var School = "𝗞𝗩 𝗡𝗼𝟭 𝗠𝗮𝗻𝗴𝗮𝗹𝗼𝗿𝗲";
+var Class = "𝟏𝟎";
+var Rollno = "23";
+var Subject1 ="𝕄𝕒𝕥𝕙𝕤";
+var Subject2 ="𝕊𝕔𝕚𝕖𝕟𝕔𝕖";
+var Subject3 ="𝕊𝕠𝕔𝕚𝕒𝕝 𝕊𝕥𝕦𝕕𝕚𝕖𝕤";
+var Mark1 = 67;
+var Mark2 = 78;
+var Mark3 = 81;
+console.log("ＮＡＭＥ："+ " "+ name)
+console.log("ＣＬＡＳＳ："+" "+Class)
+console.log("ＲＯＬＬ ＮＯ："+" "+Rollno)
+console.log("ＳＣＨＯＯＬ："+" "+School)
+console.log("𝑺𝑼𝑩𝑱𝑬𝑪𝑻𝑺 𝑨𝑵𝑫 𝑴𝑨𝑹𝑲𝑺")
+console.log(Subject1+" "+ "Mark:"+ " "+Mark1)
+console.log(Subject2+" "+ "Mark:"+ " "+Mark2)
+console.log(Subject3+" "+ "Mark:"+ " "+Mark2)
