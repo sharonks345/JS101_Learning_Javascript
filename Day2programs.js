30d2cb5bcceceecaed236a94996bdf0d7803bb00
sharonks345 <sharonks1995@gmail.com>
2024-01-09 19:44:43 +0530

    new files

---
+++ Day2programs.js
@@ -0,0 +1,56 @@
+// Problem 1 (Multiply by 50)
+// You are given a number stored in variable, with the name number
+// Multiply the number stored in the variale number by 50 and print the result obtained.
+
+let number = 35;
+console.log(number*50)
+
+//Problem 2 (Product of six)
+//You are given six numbers stored in variables with the following names
+//one,two,three,four,five,six
+// Find the product of all six
+
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six =6;
+var product = one*two*three*four*five*six;
+console.log(product)
+
+// Problem 3 (Sum of five)
+// You are given five numbers stored in variables with the following names
+// one , two , three , four , five 
+// Find the sum of the five values and print it
+
+var one = 21;
+var two = 22;
+var three = 23;
+var four = 24;
+var five = 25;
+console.log(one + two + three + four + five)
+
+// Problem 4 (Cube of a number)
+// Given a number,find its cube
+
+var num = 3;
+console.log('The cube of number '+num+' is '+num**3)
+
+// Problem 5 (Square and Sum)
+// You are given three numbers, stored in variables with the following names
+// one, two, three
+// You have to find the square of three numbers and find the sum of their squared values
+
+var one = 2;
+var two = 3;
+var three = 4;
+console.log('Final value: '+(one**2+two**2+three**2))
+
+// Problem 6(Find X)
+// You are given a number X, find the value if
+// 1. X is multiplied by 3
+// 2. 10 is added to the new value of X.
+
+var X = 8;
+console.log('The value is '+(X*3+10))
